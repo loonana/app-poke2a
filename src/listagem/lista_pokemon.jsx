a6d58cfae6e45b8698f58d79464deedf18ba3fd0
loonana <itsloye@gmail.com>
2024-05-06 08:31:28 -0300

    first commit

---
+++ src/listagem/lista_pokemon.jsx
@@ -0,0 +1,15 @@
+import React, {useState, useEffect} from "react"
+
+const listagem = () => (
+    <div>
+        <Ul>
+            <li>Pokemón 1</li>
+            <li>Pokemón 2</li>
+            <li>Pokemón 3</li>
+            <li>Pokemón 4</li>
+            <li>Pokemón 5</li>
+        </Ul>
+    </div>
+)
+
+export default listagem 
